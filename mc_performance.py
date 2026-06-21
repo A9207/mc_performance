e0641cab832864585aee59397bfff74058eb3503
A9207 <amin920720@gmail.com>
2026-06-21 10:33:29 +0800

    Update mc_performance.py

--- mc_performance.py
+++ mc_performance.py
@@ -598,24 +598,26 @@ if not filtered_df.empty:
             annotation_text="Min LIMIT = 0.05 KPA",
             annotation_position="bottom right"
         )  
-        if column == "Temperature (°C)":
-            fig.add_hline(
+    elif column == "Temperature (°C)":
+
+        fig.add_hline(
             y=60,
             line_color="red",
             line_dash="dash",
             line_width=3,
             annotation_text="MAX LIMIT = 60°C",
             annotation_position="top right"
-        )
-         if column == "Temperature (°C)":
-            fig.add_hline(
-            y=10,
+    )
+
+        fig.add_hline(
+            y=0,
             line_color="yellow",
             line_dash="dash",
             line_width=3,
-            annotation_text="Min LIMIT = 10°C",
+            annotation_text="MIN LIMIT = 0°C",
             annotation_position="bottom right"
-        )
+    )
+        
         if column == "Air Flow Rate (CFM)":
             fig.add_hline(
             y=3500,
