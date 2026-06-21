dc4ba7c68c4753d592136927c87f7b799d4748a6
A9207 <amin920720@gmail.com>
2026-06-21 10:48:46 +0800

    Update mc_performance.py

--- mc_performance.py
+++ mc_performance.py
@@ -608,16 +608,6 @@ if not filtered_df.empty:
             annotation_text="MAX LIMIT = 3500 CFM",
             annotation_position="top right"
         )
-         if column == "Air Flow Rate (CFM)":
-            fig.add_hline(
-            y=500,
-            line_color="red",
-            line_dash="dash",
-            line_width=3,
-            annotation_text="Min LIMIT = 500 CFM",
-            annotation_position="bottom right"
-        )
-
 
         fig.update_layout(
             title=f"{column} Trend",
