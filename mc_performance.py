17836d6b86cd1a8a68f73548a15dad52ac0bfc13
A9207 <amin920720@gmail.com>
2026-06-21 10:59:42 +0800

    Update mc_performance.py

--- mc_performance.py
+++ mc_performance.py
@@ -626,6 +626,15 @@ if not filtered_df.empty:
             annotation_text="MAX LIMIT = 0.4 MPa",
             annotation_position="top right"
         )
+        if column == "Compressed Air Pressure (MPa)":
+            fig.add_hline(
+            y=0.2,
+            line_color="yellow",
+            line_dash="dash",
+            line_width=3,
+            annotation_text="MIN LIMIT = 0.2 MPa",
+            annotation_position="bottom right"
+        )
         if column == "Motor Ampere (A)":
             fig.add_hline(
             y=6,
